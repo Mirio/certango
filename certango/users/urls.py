ccbbd49945c0206e8ff1e0a4a383ae81ab88c241
Mirio <miriodev@gmail.com>
2023-02-25 00:31:09 +0100

    Testing new releases + CI linter fix

--- certango/users/urls.py
+++ certango/users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from certango.users.views import user_detail_view, user_redirect_view, user_update_view
 
-
 app_name = "users"
 urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
